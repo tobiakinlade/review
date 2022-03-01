4c10252dd0d62d66b7d23f609faef493f7aa837e
Oluwatobi Akinlade <oluwatobiakinlade@Oluwatobis-MBP.lan>
2022-03-01 11:12:45 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return <main>
+    <div className="container">
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+        <Review />
+      </div>
+    </div>
+  </main>;
+}
+
+export default App;
